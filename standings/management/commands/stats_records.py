8c5bb9460a1d9b37bc94383b4fc2ee4032301bc0
Darryl Hamilton <darryl@addict.net.nz>
2018-06-07 14:59:58 +1200

    Rename update task

--- standings/management/commands/stats_records.py
+++ standings/management/commands/stats_records.py
@@ -1,13 +1,17 @@
 from django.core.management.base import BaseCommand
 from standings.models import Season, Track
-
+import logging
 
 class Command(BaseCommand):
     help = 'Update season stats and track records'
 
     def handle(self, *args, **options):
+        logger = logging.getLogger(__name__)
+
         for season in Season.objects.all():
+            logger.info('Updating stats for {}'.format(season.name))
             season.update_stats()
 
         for track in Track.objects.all():
+            logger.info('Updating records for {}'.format(track.name))
             track.update_records()
